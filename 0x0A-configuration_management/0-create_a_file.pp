22f5bc5d927cb328ceb6fc7e9b9ea68c6d5df7a9
Mikiyas Eyob <mikiyase744@gmail.com>
2022-07-17 14:56:21 +0300

    update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# Creates a file in /tmp
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+#Create a file in /tmp
+
+file { 'school':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
