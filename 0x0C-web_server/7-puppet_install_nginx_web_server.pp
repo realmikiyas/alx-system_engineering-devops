f22bd00c97f1080ce2b14e258cc8b7dd4c541baa
Mikiyas Eyob <mikiyase744@gmail.com>
2022-07-30 23:27:33 +0300

    update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,19 @@
-# Installs a Nginx server
+# installs and configs nginx server
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/yonizbrown permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { 'index.nginx-debian.html':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World!',
+}
+
+exec { 'config':
+  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com/watch?v=TfgBHC5gvTI permanent;/" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+exec { 'start':
+  command  => 'sudo service nginx restart',
+  provider => 'shell',
 }
